66b66cb469da4bf8682fbc9b3ce1c26efc6b5c9c
Mishal Qamar <mishalqamar@gmail.com>
2026-01-29 19:37:34 +0000

    ci: fix Terraform VPC workflow and document backend
    - terraform-vpc.yml: add valid on triggers, workflow_dispatch, and noop job
      to resolve 'No event triggers defined in on' annotation
    - main.tf: add comment that Terraform Cloud is used for remote state
      (GitHub equivalent of GitLab http backend)

--- infrastructure/global/vpc/main.tf
+++ infrastructure/global/vpc/main.tf
@@ -1,3 +1,5 @@
+# VPC: shared network resources. Remote state on Terraform Cloud (GitHub has no
+# built-in state like GitLab's http backend; Terraform Cloud is the equivalent).
 terraform {
   required_version = ">= 1.5.0"
 
